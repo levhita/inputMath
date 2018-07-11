db51ca84dc0a3471b79cd7e875241cda10816f0b
Levhita <levhita@gmail.com>
2018-07-11 11:24:39 -0500

    tests working, jquery working, some documentation done, basic readme, but live example explains better

--- js/inputMath.js
+++ js/inputMath.js
@@ -1,27 +1,19 @@
+/** Based on https://learn.jquery.com/plugins/basic-plugin-creation/ **/
 (function($) {
- 
-    var inputMath = function(text) {
-        const regex = /^[0-9\.\+\-\(\)\/\* \t]*$/gm;
-        if ( regex.exec(text) !== null) {
-            try {
-                var evaluation = Function('"use strict";return (' + text + ')')();
-            } catch(e) {
-                console.log("Wrong Syntax!");
-                return text;
-            }
-            return evaluation;
-        }
-        return text;
-    }
     
     $.fn.inputMath = function() {
- 
+        
         this.filter('input[type="text"]').each(function() {
             $(this).on('keyup',(event)=>{
                 var $target = $(event.target);
                 if (event.which == 13) {
                     console.log("Evaluating:"+$target.val() );
-                    var new_value = inputMath($target.val());
+                    var new_value = "";
+                    try {
+                        new_value = mathEval($target.val());
+                    } catch(e) {
+                        new_value = $target.val();
+                    }
                     $target.val(new_value);
                 } else {
                     console.log($target.val());
@@ -29,7 +21,7 @@
             });
             
         });
- 
+        
         return this;
     };
     
---
+++ tests/mathEval.spec.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const mathEval = require('../js/mathEval');
+
+describe('mathEval()', () =>{
+    it('Must return 4, when given 2+2', () => {
+        assert.equal( mathEval("2+2"), 4);
+    });
+    
+    it('Must return 8, when using (2+2)*2 //parentesis', () => {
+        assert.equal( mathEval("(2+2)*2"), 8);
+    });
+
+    it('Must return 2.5, when using ((2+2) * 2 / 2) - 1.5 //all symbols allowed', () => {
+        assert.equal( mathEval("((2+2) * 2 / 2) - 1.5"), 2.5);
+    });
+
+    it('Must return 3, when using 1.5+1.5 //floating values', () => {
+        assert.equal( mathEval("1.5+1.5"), 3);
+    });
+    
+    it('Must give an error, when using invalid characters alert("vulnerable")', () => {
+        assert.throws( ()=> mathEval('alert("vulnerable")'), Error );
+    });
+});
