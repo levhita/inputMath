0ccf058fff6a9ac6641ff7952ed7ed03c808c58e
Levhita <levhita@gmail.com>
2018-07-19 12:21:01 -0500

    moved fake DOM setup to the test

--- src/inputMath.js
+++ src/inputMath.js
@@ -1,64 +1,62 @@
+(function(){
 
-if (typeof window =='undefined') {
-    /**
-    * boilerplate to allow the use of jQuery that needs a valid
-    * window and document in the test.
-    **/
-    var jsdom = require('jsdom');
+    function loader($) {
 
-    const { JSDOM } = jsdom;
-    const { window } = new JSDOM();
-    const { document } = (new JSDOM('')).window;
-    global.document = document;
-
-    var jQuery = require('jquery')(window);
-}
-
-(function( $ ) {
-    $.fn.mathEval = function(text) {
-        /** As we are running unsafe code from the user, we should allow only a limited set of
-        * characters, try this regex in: https://regex101.com/
-        **/
-        var regex = /^[0-9\.\+\-\(\)\/\* \t]*$/gm;
-        if (regex.exec(text) == null) {
-            throw new SyntaxError('Invalid Characters');
-        }
-        try {
-            /** Instead of eval, we use the alternative given by MDN:
-            * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval#Do_not_ever_use_eval!
+        $.fn.mathEval = function(text) {
+            /** As we are running unsafe code from the user, we should allow only a limited set of
+            * characters, try this regex in: https://regex101.com/
             **/
-            return Function('"use strict";return (' + text + ')')();
-        } catch (e) {
-            /** We might still have wrong syntax even with the limited set of character ((((4..4+45) **/
-            throw new SyntaxError('Wrong Syntax');
-        }
-    };
-    
-    $.fn.inputMath = function() {
+            var regex = /^[0-9\.\+\-\(\)\/\* \t]*$/gm;
+            if (regex.exec(text) == null) {
+                throw new SyntaxError('Invalid Characters');
+            }
+            try {
+                /** Instead of eval, we use the alternative given by MDN:
+                * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval#Do_not_ever_use_eval!
+                **/
+                return Function('"use strict";return (' + text + ')')();
+            } catch (e) {
+                /** We might still have wrong syntax even with the limited set of character ((((4..4+45) **/
+                throw new SyntaxError('Wrong Syntax');
+            }
+        };
         
-        this.filter('input[type="text"]').each(function() {
-            $(this).on('keyup',(event)=>{
-                var $target = $(event.target);
-                if (event.which == 13) {
-                    console.log("Evaluating:"+$target.val() );
-                    var new_value = "";
-                    try {
-                        new_value = $.fn.mathEval($target.val());
-                    } catch(e) {
-                        new_value = $target.val();
-                        console.log(e);
+        $.fn.inputMath = function() {
+            
+            this.filter('input[type="text"]').each(function() {
+                $(this).on('keyup',(event)=>{
+                    var $target = $(event.target);
+                    if (event.which == 13) {
+                        console.log("Evaluating:"+$target.val() );
+                        var new_value = "";
+                        try {
+                            new_value = $.fn.mathEval($target.val());
+                        } catch(e) {
+                            new_value = $target.val();
+                            console.log(e);
+                        }
+                        $target.val(new_value);
+                    } else {
+                        console.log($target.val());
                     }
-                    $target.val(new_value);
-                } else {
-                    console.log($target.val());
-                }
+                });
+                
             });
             
-        });
+            return this;
+        };
         
-        return this;
-    };
+    }
 
-    if (typeof window =='undefined') module.exports = $.fn.mathEval;
-
-}( jQuery ));
+    if (typeof window =='undefined'){ 
+        module.exports = function(jQuery){
+            /** Loading for Node **/
+            loader(jQuery);
+            return jQuery;
+        };
+    } else {
+        /** Loading for Browser **/
+        loader(jQuery);
+    }
+    
+})();
--- test/inputMath.spec.js
+++ test/inputMath.spec.js
@@ -1,24 +1,33 @@
 const assert  = require('assert');
-const mathEval = require('../lib/inputMath');
+
+/** Builds a fake DOM for jQuery */
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('<input type="text" value="2+2"/>')).window;
+global.document = document;
+var jQuery = require('jquery')(window);
+const $ = require('../lib/inputMath')(jQuery);
+console.log('lskdjflksjdfsdfsdfsdf', jQuery);
 
 describe('mathEval()', () =>{
     it('Must return 4, when given 2+2', () => {
-        assert.equal( mathEval("2+2"), 4);
+        assert.equal( $.fn.mathEval("2+2"), 4);
     });
     
     it('Must return 8, when using (2+2)*2 //parentesis', () => {
-        assert.equal( mathEval("(2+2)*2"), 8);
+        assert.equal( $.fn.mathEval("(2+2)*2"), 8);
     });
 
     it('Must return 2.5, when using ((2+2) * 2 / 2) - 1.5 //all symbols allowed', () => {
-        assert.equal( mathEval("((2+2) * 2 / 2) - 1.5"), 2.5);
+        assert.equal( $.fn.mathEval("((2+2) * 2 / 2) - 1.5"), 2.5);
     });
 
     it('Must return 3, when using 1.5+1.5 //floating values', () => {
-        assert.equal( mathEval("1.5+1.5"), 3);
+        assert.equal( $.fn.mathEval("1.5+1.5"), 3);
     });
     
     it('Must give an error, when using invalid characters alert("vulnerable")', () => {
-        assert.throws( ()=> mathEval('alert("vulnerable")'), Error );
+        assert.throws( ()=> $.fn.mathEval('alert("vulnerable")'), Error );
     });
 });
